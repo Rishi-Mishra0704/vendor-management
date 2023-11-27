a0ea037f12882545cf59e77125c46a50915d5427
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-11-27 17:35:47 +0530

    refactor models and serializers into its own apps

---
+++ vendorManagement/purchaseApi/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from vendorManagement.vendorApi.models import Vendor
+
+# Create your models here.
+
+
+class PurchaseOrder(models.Model):
+    po_number = models.CharField(max_length=50, unique=True)
+    vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
+    order_date = models.DateTimeField()
+    delivery_date = models.DateTimeField()
+    items = models.JSONField()
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=20)
+    quality_rating = models.FloatField(null=True, blank=True)
+    issue_date = models.DateTimeField()
+    acknowledgment_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"PO #{self.po_number} - {self.vendor.name}"
--- vendorManagement/vendorManagement/settings.py
+++ vendorManagement/vendorManagement/settings.py
@@ -38,7 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'api',
+    'vendorApi',
+    'purchaseApi',
+    'historyApi',
 ]
 
 MIDDLEWARE = [
