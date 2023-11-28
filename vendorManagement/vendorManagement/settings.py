29697cb425f87c36e7279bb05ec764f4e2e83ee6
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-11-28 19:53:39 +0530

    change db configs

--- vendorManagement/vendorManagement/settings.py
+++ vendorManagement/vendorManagement/settings.py
@@ -78,10 +78,14 @@ WSGI_APPLICATION = 'vendorManagement.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'vendors',
+       'USER': 'rishi',
+       'PASSWORD': '1111',
+       'HOST': 'localhost',
+       'PORT': '5432',
+   }
 }
 
 
