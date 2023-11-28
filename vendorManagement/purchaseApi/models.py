8970244c5c9647a41595953742aff162985be703
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-11-29 02:27:34 +0530

    add acknowledgement

--- vendorManagement/purchaseApi/models.py
+++ vendorManagement/purchaseApi/models.py
@@ -1,3 +1,4 @@
+from datetime import timedelta
 from django.db import models
 from vendorApi.models import Vendor
 
@@ -14,7 +15,30 @@ class PurchaseOrder(models.Model):
     status = models.CharField(max_length=20)
     quality_rating = models.FloatField(null=True, blank=True)
     issue_date = models.DateTimeField()
-    acknowledgment_date = models.DateTimeField(null=True, blank=True)
+    acknowledgment_date = models.DateTimeField(null=True, blank=True, default=None)
+
+    def recalculate_average_response_time(self):
+        # Get all acknowledged purchase orders for the same vendor
+        acknowledged_orders = PurchaseOrder.objects.filter(
+            vendor=self.vendor,
+            acknowledgment_date__isnull=False
+        ).exclude(id=self.id)
+
+        total_response_time = timedelta()
+
+        # Calculate the total response time
+        for order in acknowledged_orders:
+            total_response_time += order.acknowledgment_date - order.order_date
+
+        # Calculate the average response time
+        if acknowledged_orders.exists():
+            average_response_time = total_response_time / len(acknowledged_orders)
+        else:
+            average_response_time = timedelta()
+
+        # Update the vendor's average_response_time field (adjust this based on your Vendor model)
+        self.vendor.average_response_time = average_response_time.total_seconds()
+        self.vendor.save()
 
     def __str__(self):
         return f"PO #{self.po_number} - {self.vendor.name}"
--- vendorManagement/purchaseApi/views.py
+++ vendorManagement/purchaseApi/views.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from django.shortcuts import get_object_or_404
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework import status
@@ -6,11 +8,13 @@ from .models import PurchaseOrder
 from .serializer import PurchaseOrderSerializer
 # Create your views here.
 
+
 @api_view(['GET'])
 def purchase_orders(request):
     purchaseOrders = PurchaseOrder.objects.all()
     serializers = PurchaseOrderSerializer(purchaseOrders, many=True)
     return Response(serializers.data)
+
 
 @api_view(['POST'])
 def purchase_order_create(request):
@@ -20,7 +24,7 @@ def purchase_order_create(request):
         return Response(serializer.data)
     else:
         return Response(serializer.errors)
-    
+
 
 @api_view(['GET', 'PUT', 'DELETE'])
 def purchase_order_detail(request, po_id):
@@ -43,30 +47,18 @@ def purchase_order_detail(request, po_id):
     elif request.method == 'DELETE':
         purchaseOrder.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
-    
+
 
 @api_view(['POST'])
 def acknowledge_purchase_order(request, po_id):
-    try:
-        purchase_order = PurchaseOrder.objects.get(pk=po_id)
-    except PurchaseOrder.DoesNotExist:
-        return Response({"error": "Purchase Order not found"}, status=status.HTTP_404_NOT_FOUND)
+    purchase_order = get_object_or_404(PurchaseOrder, pk=po_id)
 
-    # Acknowledge Purchase Order
+    if request.method == 'POST':
+            purchase_order.acknowledgment_date = datetime.now()
+            purchase_order.save()
+            purchase_order.recalculate_average_response_time()
 
-    # Update Average Response Time
-    vendor = purchase_order.vendor
-    acknowledged_pos = PurchaseOrder.objects.filter(vendor=vendor, acknowledgment_date__isnull=False)
-    total_response_time = sum([(po.acknowledgment_date - po.issue_date).total_seconds() for po in acknowledged_pos])
-    vendor.average_response_time = total_response_time / acknowledged_pos.count() if acknowledged_pos.count() > 0 else 0
-    vendor.save()
+            serializer = PurchaseOrderSerializer(purchase_order)
+            return Response(serializer.data, status=status.HTTP_200_OK)
 
-    # Update Fulfillment Rate
-    completed_pos = PurchaseOrder.objects.filter(vendor=vendor, status='completed')
-    successful_fulfillment_pos = completed_pos.exclude(quality_rating=None)  # Modify condition based on your definition of successful fulfillment
-    
-    vendor.fulfillment_rate = (successful_fulfillment_pos.count() / completed_pos.count()) * 100
-    vendor.save()
-
-    serializer = PurchaseOrderSerializer(purchase_order)
-    return Response(serializer.data)
+    return Response({'error': 'Invalid request method.'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
