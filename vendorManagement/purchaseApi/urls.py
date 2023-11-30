32fd3de3d9e8280b8d60e3b788d50142605cda34
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-01 03:20:23 +0530

    add tests for purchaseApi

--- vendorManagement/purchaseApi/urls.py
+++ vendorManagement/purchaseApi/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('purchase_orders/', views.purchase_orders),
-    path('purchase_orders/create/', views.purchase_order_create),
-    path('purchase_orders/<int:po_id>/', views.purchase_order_detail),
-    path('purchase_orders/<int:po_id>/acknowledge/', views.acknowledge_purchase_order),
+    path('purchase_orders/', views.purchase_orders, name='purchase_orders'),
+    path('purchase_orders/create/', views.purchase_order_create, name='purchase_order_create'),
+    path('purchase_orders/<int:po_id>/', views.purchase_order_detail, name='purchase_order_detail'),
+    path('purchase_orders/<int:po_id>/acknowledge/', views.acknowledge_purchase_order, name='acknowledge_purchase_order'),
 ]
