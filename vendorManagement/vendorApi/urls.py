adc8388119fe4a961ae7c05970422d2a14207093
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-01 01:18:06 +0530

    write tests for vendors api

--- vendorManagement/vendorApi/urls.py
+++ vendorManagement/vendorApi/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('vendors/', views.vendors, name='vendors'),
-    path('vendors/create', views.vendor_create, name='vendors'),
+    path('vendors/create', views.vendor_create, name='vendors_create'),
     path('vendors/<int:vendor_id>/',
          views.vendor_detail, name='vendor_detail'),
 ]
