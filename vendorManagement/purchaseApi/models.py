8c199aadf1ab41d14142eff0bf6de52ed6da46fd
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-05 02:22:37 +0530

    document PO model

--- vendorManagement/purchaseApi/models.py
+++ vendorManagement/purchaseApi/models.py
@@ -4,6 +4,28 @@ from vendorApi.models import Vendor
 
 
 class PurchaseOrder(models.Model):
+    """
+    Model representing a purchase order.
+
+    Attributes:
+    - po_number: Unique identifier for the purchase order.
+    - vendor: Foreign key referencing the vendor associated with the purchase order.
+    - order_date: Date and time when the purchase order was created.
+    - delivery_date: Date and time when the items are expected to be delivered.
+    - items: JSON field containing details of the ordered items.
+    - quantity: Quantity of items in the purchase order.
+    - status: Current status of the purchase order (e.g., pending, completed).
+    - quality_rating: Optional field for providing a quality rating for the purchase order.
+    - issue_date: Date and time when the purchase order was issued.
+    - acknowledgment_date: Date and time when the purchase order was acknowledged.
+
+    Methods:
+    - __str__: String representation of the purchase order.
+
+    Custom Save Method:
+    - Overrides the default save method to automatically set the acknowledgment_date if not provided.
+
+    """
     po_number = models.CharField(max_length=50, unique=True)
     vendor = models.ForeignKey(Vendor, on_delete=models.CASCADE)
     order_date = models.DateTimeField()
